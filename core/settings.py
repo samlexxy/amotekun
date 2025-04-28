89194d4d88e64186656c73aeaf1252c6d664af61
Samuel Ibiloye <ibiloyes@gmail.com>
2025-04-28 22:05:03 +0100

    updated ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'kp(ijhe@0t22^kj2ip_oj$&68bg*d^%^aczbm3j_8hp@z_3^7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
