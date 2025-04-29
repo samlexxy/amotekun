349c52730592ddace91dccec67fe9ae0afe2b775
Samuel Ibiloye <ibiloyes@gmail.com>
2025-04-29 16:21:12 +0100

    removed email sending line in the contact view pending stmp setup

--- contact/views.py
+++ contact/views.py
@@ -8,15 +8,15 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            contact = form.save()
-            full_message = f"Message from {contact.name} ({contact.email}):\n\n{contact.message}"
+            # contact = form.save()
+            # full_message = f"Message from {contact.name} ({contact.email}):\n\n{contact.message}"
 
-            send_mail(
-                subject="New Contact Form Message",
-                message=full_message,
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[settings.CONTACT_EMAIL],
-            )
+            # send_mail(
+            #     subject="New Contact Form Message",
+            #     message=full_message,
+            #     from_email=settings.DEFAULT_FROM_EMAIL,
+            #     recipient_list=[settings.CONTACT_EMAIL],
+            # )
 
             messages.success(request, 'Your message has been sent successfully! Thank you!')
             return render(request, 'contact/contact.html', {'form': ContactForm()})
